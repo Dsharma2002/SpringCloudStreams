09c3ff6992a82c4a599be4ffb0885c759d92765a
Divyansh Sharma <divyansh.abbott9712@gmail.com>
2025-08-09 15:55:21 +0100

    create topics

---
+++ consumer/src/main/java/com/demo/consumer/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.demo.consumer;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic creaNewTopic() {
+        return new NewTopic("my-new-topic", 3, (short) 1);
+    }
+}
